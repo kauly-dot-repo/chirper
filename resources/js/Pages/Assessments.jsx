f4a776c073009e9aa7448227d01135c01b29b550
kauly-dot-repo <angelapeter2000@gmail.com>
2022-09-21 22:28:17 +0200

    Initial Commit

---
+++ resources/js/Pages/Assessments.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Assessments(props) {
+    return (
+        <div>
+            <h1>Assessments</h1>
+        </div>
+    );
+}
+
+export default Assessments;
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/reports', function () {
+    return Inertia::render('Reports');
+})->middleware(['auth','verified'])->name('reports');
+
+Route::get('/my-assessments', function () {
+    return Inertia::render('Assessments');
+})->middleware(['auth','verified'])->name('my-assessments');
+
+require __DIR__.'/auth.php';
